a2884a331168521405c5222fbf9c279c876933a6
Josue Igiraneza <igiranezaj28@gmail.com>
2025-09-09 11:07:22 +0200

    function that takes a string and reverses it

---
+++ src/main/java/codewars/ReverseString.java
@@ -0,0 +1,17 @@
+package codewars;
+
+public class ReverseString {
+
+    public static String reverseString(String str){
+        String reversedString = "";
+        for(int i = str.length() - 1; i >= 0; i--) {
+            reversedString += str.charAt(i);
+        }
+        return reversedString;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverseString("hello"));
+        System.out.println(reverseString("world"));
+    }
+}
