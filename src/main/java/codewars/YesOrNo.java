902a6239694216c8ec71779c1714d1b855d16e01
Josue Igiraneza <igiranezaj28@gmail.com>
2025-09-10 16:25:09 +0200

    function that transforms a boolean into yes or no.

---
+++ src/main/java/codewars/YesOrNo.java
@@ -0,0 +1,14 @@
+package codewars;
+
+public class YesOrNo {
+
+    public static String boolToWord(boolean b) {
+        return b ? "Yes" : "No";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(boolToWord(false));
+        System.out.println(boolToWord(true));
+        System.out.println(boolToWord(false));
+    }
+}
