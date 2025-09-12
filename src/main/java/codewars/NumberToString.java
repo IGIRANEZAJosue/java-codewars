fa6a38ce2485559a1f2dd65a9977af0375470eb6
Josue Igiraneza <igiranezaj28@gmail.com>
2025-09-12 18:04:05 +0200

    function that turns a number into a string

---
+++ src/main/java/codewars/NumberToString.java
@@ -0,0 +1,15 @@
+package codewars;
+
+public class NumberToString {
+
+    public static String numberToString(int num) {
+        return Integer.toString(num);
+        // You can also use String.valueOf(num);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(numberToString(999));
+        System.out.println(numberToString(123));
+        System.out.println(numberToString(-100));
+    }
+}
