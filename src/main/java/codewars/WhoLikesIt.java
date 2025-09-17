adf8db57e588b9f148563d00df9d0efedebee269
Josue Igiraneza <igiranezaj28@gmail.com>
2025-09-17 19:58:44 +0200

    function that takes names and return the people who like an item

---
+++ src/main/java/codewars/WhoLikesIt.java
@@ -0,0 +1,34 @@
+/*
+* You probably know the "like" system from Facebook and other pages. People can "like" blog posts, pictures or other items. We want to create the text that should be displayed next to such an item.
+
+Implement the function which takes an array containing the names of people that like an item. It must return the display text as shown in the examples:
+
+    []                                -->  "no one likes this"
+    ["Peter"]                         -->  "Peter likes this"
+    ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
+    ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
+    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+    Note: For 4 or more names, the number in "and 2 others" simply increases.
+* */
+
+package codewars;
+
+public class WhoLikesIt {
+
+    public static String whoLikesIt(String... names) {
+        return switch (names.length){
+           case 0 -> "no one likes this";
+           case 1 -> String.format("%s likes this", names[0]);
+           case 2 -> String.format("%s and %s like this", names[0], names[1]);
+           case 3 -> String.format( "%s, %s and %s like this", names[0], names[1], names[2]);
+           default -> String.format( "%s, %s and %d others like this", names[0], names[1], names.length - 2);
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(whoLikesIt("Alex", "Jacob", "Mark", "Max"));
+        System.out.println(whoLikesIt("Max", "John", "Mark"));
+        System.out.println(whoLikesIt("Alex", "Jacob", "Mark", "Max", "Josue", "Juan", "Fernandez"));
+    }
+}
