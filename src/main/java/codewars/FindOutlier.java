657dae5b73b53c8888873d5611536c71bf79126c
josue.igiraneza <josue.igiraneza@bestseller.com>
2025-10-07 19:23:29 +0200

    function that finds the odd or even outlier in an array of either odd or even numbers

---
+++ src/main/java/codewars/FindOutlier.java
@@ -0,0 +1,29 @@
+/*
+ You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+ The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N. Write a method that takes the array as an argument and returns this "outlier" N.
+
+    Examples
+    [2, 4, 0, 100, 4, 11, 2602, 36] -->  11 (the only odd number)
+
+    [160, 3, 1719, 19, 11, 13, -21] --> 160 (the only even number)
+* */
+
+package codewars;
+
+import java.util.Arrays;
+
+public class FindOutlier {
+
+    static int find(int[] integers) {
+        int[] odds = Arrays.stream(integers).filter(n -> n % 2 != 0).toArray();
+        int[] evens = Arrays.stream(integers).filter(n -> n % 2 == 0).toArray();
+
+        return odds.length > 1 ? evens[0] : odds[0];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(find(new int[] {2, 6, 8, -10, 3}));
+        System.out.println(find(new int[] {2, 4, 0, 100, 4, 11, 2602, 36}));
+        System.out.println(find(new int[]{160, 3, 1719, 19, 11, 13, -21}));
+    }
+}
