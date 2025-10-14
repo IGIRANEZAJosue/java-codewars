eb33928d851803731d9eade544de6163452b56e5
josue.igiraneza <josue.igiraneza@bestseller.com>
2025-10-14 20:17:09 +0200

    function that computes the difference between 2 arrays/lists

---
+++ src/main/java/codewars/ArrayDifference.java
@@ -0,0 +1,30 @@
+/*
+* Implement a function that computes the difference between two lists.
+* The function should remove all occurrences of elements from the first list (a) that are present in the second list (b).
+* The order of elements in the first list should be preserved in the result.
+
+Examples
+    If a = [1, 2] and b = [1], the result should be [2].
+
+    If a = [1, 2, 2, 2, 3] and b = [2], the result should be [1, 3].
+* */
+
+package codewars;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class ArrayDifference {
+
+    public static int[] arrayDiff(int[] a, int[] b) {
+        Set<Integer> bSet = Arrays.stream(b).boxed().collect(Collectors.toSet());
+
+        return Arrays.stream(a).filter(n -> !bSet.contains(n)).toArray();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(arrayDiff(new int [] {1,2}, new int[] {1})));
+        System.out.println(Arrays.toString(arrayDiff(new int [] {1, 2, 2, 2, 3}, new int[] {2})));
+    }
+}
